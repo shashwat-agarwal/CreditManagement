d3f981ee02d4a5538097140acf92ac708051fb64
Shashwat Agarwal <shashwat015@users.github.com>
2020-07-20 00:50:22 +0530

    spinner added

--- app/src/main/java/com/example/creditmanagement/TransactionActivity.java
+++ app/src/main/java/com/example/creditmanagement/TransactionActivity.java
@@ -7,14 +7,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionActivity extends AppCompatActivity {
 
@@ -22,6 +34,8 @@ public class TransactionActivity extends AppCompatActivity {
     private String id;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference userRef = db.collection("user");
+    Spinner spinner;
+    List<String> id_collection;
 
     public void transact(View view) {
         finish();
@@ -32,9 +46,46 @@ public class TransactionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
         displayDetails = findViewById(R.id.textview_display);
+        spinner=findViewById(R.id.spinner);
+
         Intent intent = getIntent();
         id = intent.getStringExtra("id");
         load();
+        addInSpinner();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(TransactionActivity.this, adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    private void addInSpinner() {
+        final List<String> names = new ArrayList<>();
+        id_collection=new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, names);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        userRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        String name_list = document.getString("name");
+                        String  id_list=document.getId();
+                        if (!id_list.equals(id))
+                        names.add(name_list);
+                    }
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 
     public void load() {
@@ -48,7 +99,7 @@ public class TransactionActivity extends AppCompatActivity {
                             String address = userInformation.getAddress();
                             String email = userInformation.getEmail();
                             int credit = userInformation.getCredit();
-                            displayDetails.setText("Name:  " + name + "\n" + "Email:  " + email+"\n"+"Address: "+address +"\n"+"Credit: "+  credit);
+                            displayDetails.setText("Name:  " + name + "\n" + "Email:  " + email + "\n" + "Address: " + address + "\n" + "Credit: " + credit);
                         } else {
                             Toast.makeText(TransactionActivity.this, "Document does not exist", Toast.LENGTH_SHORT).show();
                         }
@@ -61,5 +112,26 @@ public class TransactionActivity extends AppCompatActivity {
                         Log.d("TAG", e.toString());
                     }
                 });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    return;
+                }
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    UserInformation userInformation = documentSnapshot.toObject(UserInformation.class);
+
+                    String name = userInformation.getName();
+
+                }
+
+            }
+        });
     }
 }
